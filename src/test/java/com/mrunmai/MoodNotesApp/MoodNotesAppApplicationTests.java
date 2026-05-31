27f38eb294938976c7368d480a100df63f7f8305
MrunmaiThete <mrunmaithete.mrt2005@gmail.com>
2026-05-31 16:10:05 +0530

    Add tests

---
+++ src/test/java/com/mrunmai/MoodNotesApp/MoodNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrunmai.MoodNotesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodNotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
