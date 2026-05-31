046462f4b6c9d3b453c9a70e2effb374e46d67c1
MrunmaiThete <mrunmaithete.mrt2005@gmail.com>
2026-05-31 16:07:22 +0530

    Security config

---
+++ src/main/java/com/mrunmai/MoodNotesApp/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.mrunmai.MoodNotesApp.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI myCustomConfig(){
+        return new OpenAPI()
+                .info(
+                new Info().title("MoodNotes App APIs")
+                        .description("By Mrunmai")
+                )
+                .servers(Arrays.asList(
+                        new Server().url("http://localhost:8080/moodnotes").description("local"),
+                        new Server().url("http://localhost:8080/moodnotes").description("live")))
+                .tags(Arrays.asList(
+                        new Tag().name("Public APIs"),
+                        new Tag().name("User APIs"),
+                        new Tag().name("MoodNotes APIs"),
+                        new Tag().name("Admin APIs")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes(
+                        "bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                ));
+    }
+}
