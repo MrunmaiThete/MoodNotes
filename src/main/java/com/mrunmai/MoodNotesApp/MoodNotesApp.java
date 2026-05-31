7f975100fb65fe18ffaa702e36b6d03be632be78
MrunmaiThete <mrunmaithete.mrt2005@gmail.com>
2026-05-31 16:09:52 +0530

    Main class

---
+++ src/main/java/com/mrunmai/MoodNotesApp/MoodNotesApp.java
@@ -0,0 +1,31 @@
+package com.mrunmai.MoodNotesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableScheduling
+public class MoodNotesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodNotesApp.class, args);
+    }
+
+    @Bean
+    public PlatformTransactionManager transaction(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+        
+    }
+}
